d394253151a615338243984b3f2a3420873ff723
Chad Carino <chadc@Chad-Carino-x-379.local>
2013-06-05 20:29:51 -0400

    new plugin, loose permissions

--- wp-config.php
+++ wp-config.php
@@ -25,7 +25,7 @@ define('DB_USER', 'wordpress');
 define('DB_PASSWORD', 'r7h3bo10');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'chad-carino-x-379.local');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
