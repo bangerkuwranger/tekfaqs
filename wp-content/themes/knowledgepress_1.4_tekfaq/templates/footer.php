b7367137d3cbacba0d224ef7abfca49ac4210184
Chad A. Carino <chadc@tekserve.com>
2014-04-09 18:56:34 -0400

    Removed Feedback tab

--- wp-content/themes/knowledgepress_1.4_tekfaq/templates/footer.php
+++ wp-content/themes/knowledgepress_1.4_tekfaq/templates/footer.php
@@ -25,12 +25,3 @@ piCId = '1096';
 	else { window.addEventListener('load', async_load, false); }
 })();
 </script>
-<script type="text/javascript">
-var fby = fby || [];
-fby.push(['showTab', {id: '6779', position: 'left', color: '#40A8C9'}]);
-(function () {
-    var f = document.createElement('script'); f.type = 'text/javascript'; f.async = true;
-    f.src = '//cdn.feedbackify.com/f.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(f, s);
-})();
-</script>
